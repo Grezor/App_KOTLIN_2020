79b089b7d481436c68ec65c14d3d1b526fde00f2
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-02-25 19:43:33 +0100

    add image default

--- app/src/main/java/fr/duplessigeoffrey/choptaphoto/ppe_android/PhotoAdapter.kt
+++ app/src/main/java/fr/duplessigeoffrey/choptaphoto/ppe_android/PhotoAdapter.kt
@@ -1,9 +1,11 @@
 package fr.duplessigeoffrey.choptaphoto.ppe_android
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.photo_row.view.*
 
 class PhotoViewHolder(v : View) : RecyclerView.ViewHolder(v){
@@ -30,6 +32,14 @@ class PhotoAdapter(val photos: List<Photo>): RecyclerView.Adapter<PhotoViewHolde
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
         //
+        when(position % 3){
+            0 -> holder.itemView.setBackgroundColor(Color.parseColor("#f99999"))
+            1 -> holder.itemView.setBackgroundColor(Color.parseColor("#99ff99"))
+            2 -> holder.itemView.setBackgroundColor(Color.parseColor("#9999ff"))
+        }
         holder.titleTextView.text = "Photo : ${photos[position].id}"
+        // on ajoute une photo basic
+        val imageUrl = "https://cdn.shopify.com/s/files/1/1087/6636/products/Captain-America-Icon_01_Top-View_600x600.png?v=1558444338"
+        Glide.with(holder.photoImageView).load(imageUrl).into(holder.photoImageView)
     }
 }
