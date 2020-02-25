a088f9640810b3efafc75a4c1f76ca3b9826e265
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-02-25 11:23:06 +0100

    add permission to manifest

--- app/src/main/java/fr/duplessigeoffrey/choptaphoto/ppe_android/MainActivity.kt
+++ app/src/main/java/fr/duplessigeoffrey/choptaphoto/ppe_android/MainActivity.kt
@@ -8,6 +8,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.Executors
+import kotlin.math.log
 
 class Photo(val id:Int, val url: String)
 // ajouter
@@ -25,16 +27,27 @@ class MainActivity : AppCompatActivity() {
         val retrofit = Retrofit.Builder()
             // le serveur api
             .baseUrl("https://grezor.alwaysdata.net/")
+            // apel dependance mochi
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
-
-
         // instensier apel api
         val photoService = retrofit.create(PhotoService::class.java)
         // instancie l'apel de photo.php avec le code SONA
         val photoCall = photoService.listPhotos("SONA")
-        //recupere nos photo
-        val photos = photoCall.execute()
-        Log.d("APP", "${photos.body()?.size}")
+
+        // thread, elle crée un nouveau thread, avec la possibilité d'instruction
+        val executors = Executors.newSingleThreadExecutor()
+        executors.submit {
+            try {
+                //recupere nos photo
+                val photos = photoCall.execute()
+                Log.d("APP", "${photos.body()?.size}") // affiche 2
+            }catch (e: Exception){
+                Log.e("APP", e.message)
+            }
+
+
+        }
+
     }
 }
